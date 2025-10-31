95ffe5f5b65b63b6fcbb06e68c9afff58ab08eac
Eric Morrison <eric_m250@outlook.com>
2025-10-31 15:09:51 -0700

    set up authcontroller and authroutes with register and login

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,9 +1,39 @@
 import { Request, Response } from 'express'
+import { register, login } from '../services/authService'
 
 export const registerUser = async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        return res.status(400).json({ error: 'Email and password are required' })
+    }
+
+    try{
+        const user = await register(email, password)
+        res.status(201).json({ user })
+    } catch (err) {
+        if (err instanceof Error && err.message === 'User already exists') {
+            return res.status(409).json({ error: 'User already exists' });
+        }
+        res.status(500).json({ error: 'Internal server error' });
+    }
 
 }
 
-export const loginUser = async (req, res) => {
+export const loginUser = async (req: Request, res: Response) => {
+    const { email, password } = req.body
+    if(!email || !password){
+        return res.status(400).json({ error: 'Email and password required'})
+    }
 
+    try{
+        const result = await login(email, password)
+        return res.status(200).json({ user: result.user, token: result.token })
+
+    } catch (err) {
+        if (err instanceof Error && err.message === 'Invalid email or password') {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+        res.status(500).json({ error: 'Internal server error' });
+    }
 }
